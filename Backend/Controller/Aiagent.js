fc9235b8ee9d019e98afb300c89c2dab6ca143eb
prathmesh-sargar <sargarprathmesh007@gmail.com>
2026-03-04 01:13:15 +0530

    backend ai agent fixed

--- Backend/Controller/Aiagent.js
+++ Backend/Controller/Aiagent.js
@@ -51,7 +51,7 @@ const generateAIResponse = async (req, res) => {
 };
 const generateGeminiResponse = async (data,resumedata, question, username) => {
   const genAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY3);
-  const model = genAI.getGenerativeModel({ model: "gemini-2.5-flash" });
+  const model = genAI.getGenerativeModel({ model: "gemini-3-flash-preview" });
 
   const prompt = `
 You are an AI assistant analyzing a user's coding profile.
